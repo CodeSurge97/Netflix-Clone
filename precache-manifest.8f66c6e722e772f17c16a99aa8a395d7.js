689cc1404b34f0712d4cf344005e6d388d6b5465
Serge Metellus <metellusserge@gmail.com>
2021-02-12 01:43:17 -0500

    Updates

---
+++ precache-manifest.8f66c6e722e772f17c16a99aa8a395d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9224d88d70b9bbb5c46797619dd7fe",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "020f416b8bc07764db87",
+    "url": "/my-app/static/css/main.0fc5037c.chunk.css"
+  },
+  {
+    "revision": "7c54a0619fd72a36d669",
+    "url": "/my-app/static/js/2.24fc8d59.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/my-app/static/js/2.24fc8d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "020f416b8bc07764db87",
+    "url": "/my-app/static/js/main.9ce73eb7.chunk.js"
+  },
+  {
+    "revision": "8ada5a3e232b7a98b576",
+    "url": "/my-app/static/js/runtime-main.607f1f99.js"
+  }
+]);
